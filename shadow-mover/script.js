c152ded3c2ab20fba8f06b5b728502cee080b2c8
Mohd Saquib <ans2sqb@gmail.com>
2020-02-16 23:28:37 +0530

    finished

--- shadow-mover/script.js
+++ shadow-mover/script.js
@@ -4,18 +4,22 @@ const h = document.querySelector("h1")
 function mover(e) {
     const width = header.offsetWidth;
     const height = header.offsetHeight;
-    const walk = 100;
+    const walk = 150;
     let x = e.offsetX;
     let y = e.offsetY;
 
-    const xWalk = (x / width * walk) - walk / 2;
-    const yWalk = (y / width * walk) - walk / 2;
+    const xWalk = Math.round((x / width * walk) - (walk / 2));
+    const yWalk = Math.round((y / width * walk) - (walk / 2));
 
 
 
     console.log(xWalk, yWalk)
 
-    h.style.textShadow = `${xWalk}px ${yWalk}px rgba(255,0,0,0.6)`;
+    h.style.textShadow = `
+    ${xWalk}px ${yWalk}px rgba(255,0,0,0.6), 
+    ${xWalk*-1}px ${yWalk}px rgba(0,255,0,0.6),
+    ${yWalk}px ${xWalk*-1}px rgba(0,0,255,0.6),
+    ${yWalk*-1}px ${xWalk}px rgba(255,255,0,0.6)
+    `;
 }
-
 header.addEventListener('mousemove', mover)
